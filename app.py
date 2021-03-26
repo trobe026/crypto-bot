9c8ab6d4fa0b32716b820ac7160d8af4545adff6
Travis Roberts <travis@datafiniti.co>
2021-03-26 12:21:20 -0600

    complete. buy logic and error handling

--- app.py
+++ app.py
@@ -1,30 +1,39 @@
 import config, csv
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, jsonify
 from binance.client import Client
 from binance.enums import *
 
 app = Flask(__name__)
-
+app.secret_key = 'iowjaeionvneioa390IOJIOEV(@&OIJe'
 client = Client(config.API_KEY, config.API_SECRET, tld='us')
 
 @app.route('/')
 def index():
-    title = "CoinView"
     info = client.get_account()
+
+    title = "CoinView"
     balances = info['balances']
     exchange_info = client.get_exchange_info()
     symbols = exchange_info['symbols']
+
     return render_template('index.html', title = title, my_balances=balances, exchange=exchange_info, symbols=symbols)
 
 @app.route('/buy', methods=['POST'])
 def buy():
-    print(request.form)
-    order = client.create_order(symbol='ADAUSD', side=SIDE_BUY,
-        type=ORDER_TYPE_LIMIT,
-        timeInForce=TIME_IN_FORCE_GTC,
-        quantity=100,
-        price='0.00001')
-    return 'buy'
+    try:
+        print(request.form)
+
+        order = client.create_order(
+            symbol=request.form['symbol'], 
+            side=SIDE_BUY,
+            type=ORDER_TYPE_MARKET,
+            timeInForce=TIME_IN_FORCE_GTC,
+            quantity=request.form['quantity']
+            )
+    except Exception as e:
+        flash(str(e), "error")
+
+    return redirect('/')
 
 @app.route('/sell')
 def sell():
