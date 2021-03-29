8204ffbb93fd75d5ecf56f5514c5e110030a7e5f
Travis Roberts <travis@datafiniti.co>
2021-03-29 11:47:42 -0600

    attempt local candlesticks

--- static/chart.js
+++ static/chart.js
@@ -1,14 +1,16 @@
 const chart = LightweightCharts.createChart(document.getElementById('chart'), { width: 400, height: 300 });
-const lineSeries = chart.addLineSeries();
-lineSeries.setData([
-    { time: '2019-04-11', value: 80.01 },
-    { time: '2019-04-12', value: 96.63 },
-    { time: '2019-04-13', value: 76.64 },
-    { time: '2019-04-14', value: 81.89 },
-    { time: '2019-04-15', value: 74.43 },
-    { time: '2019-04-16', value: 80.01 },
-    { time: '2019-04-17', value: 96.63 },
-    { time: '2019-04-18', value: 76.64 },
-    { time: '2019-04-19', value: 81.89 },
-    { time: '2019-04-20', value: 74.43 },
-]);
+
+const candleSeries = chart.addCandlestickSeries({
+    upColor: '#00FF00',
+    downColor: '#FF0000',
+    borderDownColor:'#800080',
+    borderUpColor:'#add8e6',
+    wickDownColor: '#ffcccb',
+    wickUpColor: '#90ee90'
+});
+
+fetch('http://localhost:5000/history')
+    .then((r) => r.json())
+    .then((response) => {
+        console.log(response)
+    });
